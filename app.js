0e310c86072927dc6b23aa08a7b66b595615a7cb
Keith Morgan <morgan.keith@live.com>
2023-03-08 07:50:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,9 @@ if(navToggle){
         navMenu.classList.add('show-menu')
     })
 }
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
