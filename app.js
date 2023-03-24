f30632994d7f3ae69d85b8030d38d40373a7a1a9
Keith Morgan <morgan.keith@live.com>
2023-03-24 13:28:06 -0700

    add modal open and close JS

--- app.js
+++ app.js
@@ -76,4 +76,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
         modalViews[modalClick].classList.add('active-modal')
     }
 
+    modalBtns.forEach((modalBtn, i) => {
+        modalBtn.addEventListener('click', () =>{
+            modal(i)
+        })
+    })
+
+    modalCloses.forEach((modalClose) => {
+        modalClose.addEventListener('click', () =>{
+            modalViews.forEach((modalView) =>{
+                modalView.classList.remove('active-modal')
+            })
+        })
+    })
+
 })
